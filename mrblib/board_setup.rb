d7efe3e0c6b684a83a89a06aeb37c53919bade39
vickash <vickashmahabir@gmail.com>
2023-06-30 17:22:41 -0400

    Define digital/adc/dac/pwm levels

--- mrblib/board_setup.rb
+++ mrblib/board_setup.rb
@@ -2,5 +2,28 @@ module Denko
   class Board
     include ESP32::Constants
     CHIP_MODEL = ESP32::System.chip_model
+    
+    HIGH = 1
+    LOW = 0
+    
+    def low
+      LOW
+    end
+    
+    def high
+      HIGH
+    end
+    
+    def pwm_high
+      255
+    end
+    
+    def adc_high
+      4095
+    end
+    
+    def dac_high
+      255
+    end    
   end
 end
